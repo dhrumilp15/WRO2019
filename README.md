65d0931fc55d3ed173d3aba43b2eb07b48ef218f
Dhrumil Patel <dhrumilp15@gmail.com>
2019-03-13 17:58:23 -0400

    Proportional Control and Intersection Detection

--- README.md
+++
@@ -1,6 +0,0 @@
-# WRO2019
-Jubjub's code for WRO 2019
-
-## Notes
-ev3dev requires that the line endings are LF, so make sure that git didn't change them to CRLF when you download the files
-Change the file path of the .venv python.exe in settings.json to the file path on your computer
--- run2019.py
+++ run2019.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, OUTPUT_C, OUTPUT_B, SpeedDPS, MoveTank, MoveSteering, SpeedPercent
+from ev3dev2.motor import LargeMotor, OUTPUT_C, OUTPUT_B, OUTPUT_D, OUTPUT_A, SpeedDPS, MoveTank, MoveSteering, SpeedPercent
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import LightSensor
 from ev3dev2.button import Button
 from time import sleep
 import sys, os
 
+from math import isnan
+
 #Set up
 btn = Button()
-LLight = LightSensor(INPUT_2)
-RLight = LightSensor(INPUT_3)
 
-drive = MoveTank(OUTPUT_B,OUTPUT_C)
-steer = MoveSteering(OUTPUT_B,OUTPUT_C)
+LLight = LightSensor(INPUT_1)
+RLight = LightSensor(INPUT_4)
 
-lbline, lbblack, lbwhite = 58.9, 0, 0
-rbline, rbblack, rbwhite = 0, 0, 0
+drive = MoveTank(OUTPUT_D,OUTPUT_A)
+steer = MoveSteering(OUTPUT_D,OUTPUT_A)
 
-minRef = 40.4
-maxRef = 65.9
+linter, rinter = 0,0
+
 os.system('setfont Lat15-TerminusBold14')
 
-def calibratelights(lbline, lbblack, lbwhite, rbline, rbblack, rbwhite):
+def calibratelights(lbline, lbblack, lbwhite, rbblack, rbwhite):
     print("Left Sensor White: ")
     while not btn.any():
         lbwhite = int(LLight.reflected_light_intensity)
@@ -42,11 +42,6 @@ def calibratelights(lbline, lbblack, lbwhite, rbline, rbblack, rbwhite):
         rbwhite = int(RLight.reflected_light_intensity)
     print(rbwhite, file = sys.stderr)
     
-    print("Right Sensor Line: ")
-    while not btn.any():
-        rbline = int(RLight.reflected_light_intensity)
-    print(rbline, file = sys.stderr)
-    
     print("Right Sensor Black: ")
     while not btn.any():
         rbblack = int(RLight.reflected_light_intensity)
@@ -58,62 +53,97 @@ def sensordata():
         print("Right Light Sensor: ", end = "", file = sys.stderr)
         print(RLight.reflected_light_intensity, file = sys.stderr)
 
-speed = 40
-
-def calcsteering(piderror, power):
-    if piderror >= 0:
-        if piderror > 100:
-            leftpower = 0
-            rightpower = power
-        else:
-            leftpower = power - ((power*piderror) / 100)
-            rightpower = power
-    else:
-        if piderror < -100:
-            leftpower = power
-            rightpower = 0
-        else:
-            leftpower = power
-            rightpower = power - ((power*piderror) / 100)
-    return (leftpower, rightpower)
+speed = 10
         
-def run(lbline, speed, maxRef, minRef): # lbline is the target value
+def run(speed):
     kp = 1
     ki = 0
     kd = 0
     integral = 0
     perror = error = 0
+    ## We turn when left and right both register black, 
     while not btn.any():
         lv = LLight.reflected_light_intensity
-        # error = lbline - (100 * (lv - minRef) / (maxRef - minRef))
-        error = lbline - lv
-        integral = 0.5 * integral + error # Not sure why the guy multiplies integral by 1/2, but going to keep it like this until I figure it out
-        derivative = lv - perror
+        rv = RLight.reflected_light_intensity
 
-        piderror = ((error * kp) + (integral * ki) + (derivative * kd))
+        error = rv - lv
+        integral += integral + error
+        derivative = lv - perror
+        # intersections = 0
+
+        piderror = (error * kp) + (integral * ki) + (derivative * kd)
+        # piderror = 0 if isnan(piderror) else piderror
+        # if abs(piderror) >= 10:
+        #     drive.off()
+        #     while not lv >= 68:
+        #         steer.on(steering = 10, speed = speed)
 
         if speed + abs(piderror) > 100:
             if piderror >= 0:
                 piderror = 100 - speed
             else:
                 piderror = speed - 100
+        
+        # if abs(piderror) >= 10:
+        #     drive.off()
+        #     steer.on_for_degrees(steering = -100, speed = speed, degrees = 50)
 
-            drive.on_for_seconds(left_speed = speed - piderror, right_speed= speed + piderror, seconds = 0.5)
+        drive.on(left_speed = speed - piderror, right_speed= speed + piderror)
         sleep(0.01)
+        perror = error
+        # if lv < 50 and rv < 50: # if both register black, which means you're at an intersection
+        #     steer.on_for_seconds(
+        #         steering = -100,
+        #         speed = speed,
+        #         seconds = 0.1
+        #     )
+        #     # while not rv < 50:
+        #     #     steer.on(steering=-100, speed=speed)
+        #     #     sleep(0.01)
+        #     # if intersections == 4:
+        #     #     steer.on_for_degrees(steering=-100, speed=speed, degrees=75)
+        #     #     sleep(0.01)
+        #     # else:
+        #     #     intersections += 1
+        # else:
+        #     piderror = (error * kp) + (integral * ki) + (derivative * kd)
+
+        #     if speed + abs(piderror) > 100:
+        #         if piderror >= 0:
+        #             piderror = 100 - speed
+        #         else:
+        #             piderror = speed - 100
+
+        #     drive.on(left_speed = speed - piderror, right_speed= speed + piderror)
+        #     sleep(0.01)
+        #     perror = error
         
         print("The P error is:", end = " ", file = sys.stderr)
         print(piderror, end = " ", file = sys.stderr)
         print("Left Light Sensor:", end = " ", file = sys.stderr)
-        print(LLight.reflected_light_intensity, file = sys.stderr)
         
-        perror = error
+        if abs(piderror) >= 10:
+            print(LLight.reflected_light_intensity, end = " ", file = sys.stderr)
+            print("Intersection!", file = sys.stderr)
+        else:
+            print(LLight.reflected_light_intensity, file = sys.stderr)
+
 
 # while not btn.any(): # Until we can get reliable bounds on our sensor data this stays, but we'll replace with try/except in final build
 #     sensordata()
 
-run(lbline,speed,maxRef, minRef)
+run(speed)
+
+def main():
+    
+    run(speed)
+    # left()
+    # right()
+    run(speed)
 
 
+if __name__ == "__main__":
+    main()
 """
 Left
 
