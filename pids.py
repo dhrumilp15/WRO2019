4dba9d55e333c10f4b625591c6f9593e2de53f24
Dhrumil Patel <dhrumilp15@gmail.com>
2019-10-31 23:32:59 -0400

    Background Threading

--- pids.py
+++ pids.py
@@ -6,37 +6,47 @@ from ev3dev2.button import Button
 from time import sleep
 import sys, os
 import math
+import threading
+import time
+class ThreadedPID(object):
+	def __init__(self):	
+		self.btn  = Button()
+		self.LLight = LightSensor(INPUT_1)
+		self.RLight = LightSensor(INPUT_4)
+		self.drive = MoveTank(OUTPUT_B,OUTPUT_C)
+		os.system('setfont Lat15-TerminusBold14')
 
-btn  = Button()
-LLight = LightSensor(INPUT_1)
-RLight = LightSensor(INPUT_4)
-drive = MoveTank(OUTPUT_B,OUTPUT_C)
-os.system('setfont Lat15-TerminusBold14')
-speed = 15
-kp = 1.25
-kd = 0
-ki = 0
-integral = 0
-perror = error = 0
-piderror = 0
-while not btn.any(): 
-    lv = LLight.reflected_light_intensity
-    rv = RLight.reflected_light_intensity
-    error = round(rv,5) - round(lv,5)
-    integral += error
-    derivative = error - perror
+		thread = threading.Thread(target = self.run, args=())
+		thread.daemon = True
+		thread.start()
+	
+	def run(self):
+		speed = 15
+		kp = 1.25
+		kd = 0
+		ki = 0
+		integral = 0
+		perror = error = 0
+		piderror = 0
+		while not btn.any(): 
+			lv = LLight.reflected_light_intensity
+			rv = RLight.reflected_light_intensity
+			error = round(rv,5) - round(lv,5)
+			integral += error
+			derivative = error - perror
 
-    piderror = (error * kp) + (integral * ki) + (derivative * kd)
-    
-    if math.isnan(piderror): # Need to figure out how to better handle NaNs
-    	piderror = perror
+			piderror = (error * kp) + (integral * ki) + (derivative * kd)
+			
+			if math.isnan(piderror): # Need to figure out how to better handle NaNs
+				piderror = perror
 
-    if speed + abs(piderror) > 100:
-        if piderror >= 0:
-            piderror = 100 - speed
-        else:
-            piderror = speed - 100
+			if speed + abs(piderror) > 100:
+				if piderror >= 0:
+					piderror = 100 - speed
+				else:
+					piderror = speed - 100
 
-    drive.on(left_speed = speed - piderror, right_speed= speed + piderror)
-	#sleep(0.01) for debugging purposes, causes errors as robot can't update values when in sleep()
-    perror = error
+			self.drive.on(left_speed = speed - piderror, right_speed= speed + piderror)
+			#sleep(0.01) for debugging purposes, causes errors as robot can't update values when in sleep()
+			perror = error
+biggo = ThreadedPID()
